065ad21573115f88f73b92798613386c7c8b84da
Vistaofootball <vistao.sports@gmail.com>
2022-05-18 13:50:19 +0200

    Updates

---
+++ static/js/924.db5a6882.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdroow=self.webpackChunkdroow||[]).push([[924],{422:function(n,u,e){e.r(u);e(2791);var r=e(819),o=e(7265),s=e(184);u.default=function(n){var u=(0,r.UO)().slug,e=(0,o.x6)(u,"slug");return e?e.component({data:e}):(0,s.jsx)("h1",{children:u})}}}]);
+//# sourceMappingURL=924.db5a6882.chunk.js.map
